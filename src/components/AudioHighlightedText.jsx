d34c8726c18940c2176559408ef0a2d2beaf5f25
vidhal elame <vidhalelame@gmail.com>
2024-10-09 15:36:03 +0100

    add scroll

--- src/components/AudioHighlightedText.jsx
+++ src/components/AudioHighlightedText.jsx
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import ReactPlayer from "react-player";
 
-
-
-
-
-const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
+const AudioHighlightedText = ({ dataAudioJson, audioUrl }) => {
   const [segments, setSegments] = useState([]);
   const [currentTime, setCurrentTime] = useState(0);
   const [highlightedText, setHighlightedText] = useState("");
   const [displayBtn, setDisplayBtn] = useState(false);
+  const textContainerRef = useRef(null);
 
   useEffect(() => {
     const getAudioData = async () => {
       try {
-        const res3 = await axios.get(
-          dataAudioJson
-        );
+        const res3 = await axios.get(dataAudioJson);
         console.log("dataAudioJson", res3.data);
         setSegments(res3.data.segments);
       } catch (error) {
@@ -36,14 +31,34 @@ const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
     }
   }, [segments, currentTime]);
 
+
+  useEffect(() => {
+    if (highlightedText !== "" && textContainerRef.current) { // si le  segment n' pas vide et le conteneur existe ?
+      const spanElements = textContainerRef.current.querySelectorAll("span");
+
+      const segmentElement = Array.from(spanElements).find((span) =>
+        span.textContent.includes(highlightedText) // return le span qui a le même contenu que le "segment encours de lecture"
+      );
+
+      if (segmentElement) {
+        const rect = segmentElement.getBoundingClientRect(); // coordonnés de l'elt
+
+        if (
+          (rect.top < 0) ||
+          (rect.bottom > textContainerRef.current.offsetHeight)
+        ) {
+          segmentElement.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    }
+  }, [highlightedText]);
+
+
   const handleDownloadAudio = async () => {
     try {
-      const response = await axios.get(
-        audioUrl,
-        {
-          responseType: "blob",
-        }
-      );
+      const response = await axios.get(audioUrl, {
+        responseType: "blob",
+      });
       console.log("response", response);
       const url = URL.createObjectURL(response.data);
       const a = document.createElement("a");
@@ -56,9 +71,6 @@ const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
     }
   };
 
-  
-  
-
   return (
     <div
       style={{
@@ -69,9 +81,9 @@ const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
         border: "1px solid #ddd",
         borderRadius: "5px",
         boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-        textAlign:"justify",
-        height:"300px",
-        overflowY:"scroll"
+        textAlign: "justify",
+        height: "300px",
+        overflowY: "scroll",
       }}
     >
       <div
@@ -83,8 +95,8 @@ const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
           url={audioUrl}
           light={true}
           playing={false}
-          onClickPreview={(e) => setDisplayBtn(true)} 
-          onPlay={() => setCurrentTime(0)} 
+          onClickPreview={(e) => setDisplayBtn(true)}
+          onPlay={() => setCurrentTime(0)}
           onProgress={(state) => {
             setCurrentTime(state.playedSeconds);
           }}
@@ -111,6 +123,7 @@ const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
         ) : null}
       </div>
       <div
+        ref={textContainerRef}
         style={{
           fontSize: "18px",
           lineHeight: "1.5",
