30b7c6bdf36ebbb8fff5551726c38518f2814f8e
vidhal elame <vidhalelame@gmail.com>
2024-10-09 13:46:11 +0100

    add button download

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,13 @@ import AudioHighlightedText from "./components/AudioHighlightedText";
 
 
 const App = () => {
+  const dataAudioJson = "https://all-in-one-backend-j2le.onrender.com/get-json";
+  const audioUrl = "https://all-in-one-backend-j2le.onrender.com/get-audio";
+
   return (
         <Routes>
         <Route path="/" element={<Navigate replace to="/home"  />} />  
-        <Route path="/home" element={<AudioHighlightedText />} />
+        <Route path="/home" element={<AudioHighlightedText dataAudioJson={dataAudioJson} audioUrl={audioUrl} />} />
       </Routes>
   );
 };
--- src/components/AudioHighlightedText.jsx
+++ src/components/AudioHighlightedText.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect } from "react";
 import ReactPlayer from "react-player";
 
 
 
-const AudioHighlightedText = () => {
+
+
+const AudioHighlightedText = ({dataAudioJson, audioUrl}) => {
   const [segments, setSegments] = useState([]);
   const [currentTime, setCurrentTime] = useState(0);
   const [highlightedText, setHighlightedText] = useState("");
+  const [displayBtn, setDisplayBtn] = useState(false);
 
   useEffect(() => {
     const getAudioData = async () => {
       try {
         const res3 = await axios.get(
-          "https://all-in-one-backend-j2le.onrender.com/get-json"
+          dataAudioJson
         );
-       // console.log("dataAudioJson", res3.data);
+        console.log("dataAudioJson", res3.data);
         setSegments(res3.data.segments);
       } catch (error) {
         console.log("error", error);
@@ -34,6 +36,29 @@ const AudioHighlightedText = () => {
     }
   }, [segments, currentTime]);
 
+  const handleDownloadAudio = async () => {
+    try {
+      const response = await axios.get(
+        audioUrl,
+        {
+          responseType: "blob",
+        }
+      );
+      console.log("response", response);
+      const url = URL.createObjectURL(response.data);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "audio.mp3";
+      a.click();
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  
+  
+
   return (
     <div
       style={{
@@ -42,7 +67,11 @@ const AudioHighlightedText = () => {
         padding: "20px",
         backgroundColor: "#f9f9f9",
         border: "1px solid #ddd",
+        borderRadius: "5px",
         boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+        textAlign:"justify",
+        height:"300px",
+        overflowY:"scroll"
       }}
     >
       <div
@@ -51,14 +80,35 @@ const AudioHighlightedText = () => {
         }}
       >
         <ReactPlayer
-          url={"https://all-in-one-backend-j2le.onrender.com/get-audio"}
-          playing={true}
+          url={audioUrl}
+          light={true}
+          playing={false}
+          onClickPreview={(e) => setDisplayBtn(true)} 
+          onPlay={() => setCurrentTime(0)} 
           onProgress={(state) => {
             setCurrentTime(state.playedSeconds);
           }}
+          onError={(e) => console.log("onError Audio", e)}
           controls
           height={60}
+          width="100%"
         />
+        {displayBtn ? (
+          <button
+            style={{
+              backgroundColor: "rgb(69, 64, 64)",
+              color: "white",
+              marginTop: "20px",
+              padding: "10px 20px",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+            onClick={handleDownloadAudio}
+          >
+            Dowload audio
+          </button>
+        ) : null}
       </div>
       <div
         style={{
@@ -72,9 +122,9 @@ const AudioHighlightedText = () => {
             style={{
               padding: "5px",
               borderRadius: "5px",
-             // transition: "background-color 0.1s ease-in-out",
+              // transition: "background-color 0.1s ease-in-out",
               backgroundColor:
-                segment.text === highlightedText ? "yellow" : "transparent",
+                segment.text === highlightedText ? "gray" : "transparent",
             }}
           >
             {segment.text}{" "}
